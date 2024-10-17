de524cdd35e7317d3cd339ade34795af29f6d8b0
Shane Wade <shanebramble96@hotmail.com>
2024-10-16 19:40:20 -0500

    succesffully captured input from user

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import { generateMarkdown } from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project title for this repository?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description for this repository",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions for this repository?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What are some usage information for this repository?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Who contributed to this repository?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What are the test instructions?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select the appropriate license for this repository",
+    choices: [
+      "MIT License",
+      "GNU General Public License (GPL)",
+      "GNU Lesser General Public License (LGPL)",
+      "Apache License 2.0",
+      "Mozilla Public License 2.0 (MPL 2.0)",
+      "BSD License",
+      "Creative Commons Licenses",
+    ],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    return generateMarkdown(answers);
+});
+
+}
 
 // Function call to initialize app
 init();
-
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,76 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // Object destructuring of inputted answers.
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    test,
+    license,
+    ...questions
+  } = data;
+
+  return `
+# ${title}
+
+## Description
+${description}
+
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#contributions)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Contributions
+
+${contribution}
+
+## License
+
+${license}
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+${test}
+
+## Questions
+Please feel free to checkout my other repositories : https://github.com/${questions.username}
+Send me an email. Email: ${questions.email};
 
 `;
 }
 
-module.exports = generateMarkdown;
+export { generateMarkdown };
