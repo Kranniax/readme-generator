14638848b1f3d5a1dbbacd778f0d05c88008cbf6
Shane Wade <shanebramble96@hotmail.com>
2024-10-17 18:29:11 -0500

    included badge for readme document

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,20 +25,22 @@ function generateMarkdown(data) {
   } = data;
 
   return `
+![License:${license} ](https://img.shields.io/badge/license-${license}-blue)
+
 # ${title}
 
 ## Description
 ${description}
 
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#contributions)
+- [Contributions](#contributions)
 - [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
@@ -55,23 +57,6 @@ ${contribution}
 ## License
 
 ${license}
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
 
@@ -79,7 +64,8 @@ ${test}
 
 ## Questions
 Please feel free to checkout my other repositories : https://github.com/${questions.username}
-Send me an email. Email: ${questions.email};
+
+For additional questions, reach me via email. Email: ${questions.email};
 
 `;
 }
