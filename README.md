e975fa741c04320958d0d5377d4a1cb14d7a5cc4
Shane Wade <shanebramble96@hotmail.com>
2024-10-21 18:27:20 -0500

    updated repo's README file

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Professional README Generator Starter Code
+# README Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+## Description
+
+A CLI (command line interface) application that creates a README.md file based on a user's input. This application is focused more on developers to aid them in quickly creating a README file for their online repository.
+
+## Requirements
+
+For development, you will only need Node.js.
+
+## Installation
+
+### Node installation on Windows
+
+Please visit [Official Node.js Website](https://nodejs.org/) and download the installer.
+Also, be sure to have `git` available in your PATH, `npm` might need it (You can find git [here](https://git-scm.com/)).
+
+To install all node modules and dependencies, please run the command. npm install
+
+## Usage
+
+![Alt text](./assets/Screenshot%202024-10-21%20182456.jpg)
+
+## Technologies Used
+
+- JavaScript
+- Node.js
+
+## Contributions
+
+- Shane Bramble-Wade
+
+## Project Status
+
+Project is: _completed_
+
+## Contact
+
+Created by [@kranniax](https://twitter.com/kranniax) - feel free to contact me!
--- index.js
+++ index.js
@@ -1,81 +1,8 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
+import { questions } from "./src/questions.js";
 import { generateMarkdown } from "./utils/generateMarkdown.js";
-
-// TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is the project title for this repository?",
-    validate: (projectTitle) => {
-      if (projectTitle) {
-        return true;
-      } else {
-        console.log("Please enter a project title!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "What is the description for this repository",
-    validate: (projectDescription) => {
-      if (projectDescription) {
-        return true;
-      } else {
-        console.log("Please enter a project description!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What are the installation instructions for this repository?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What are some usage information for this repository?",
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: "Who contributed to this repository?",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "What are the test instructions?",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please select the appropriate license for this repository",
-    choices: [
-      "MIT License",
-      "GNU General Public License (GPL)",
-      "GNU Lesser General Public License (LGPL)",
-      "Apache License 2.0",
-      "Mozilla Public License 2.0 (MPL 2.0)",
-      "BSD License",
-      "Creative Commons Licenses",
-    ],
-  },
-  {
-    type: "input",
-    name: "username",
-    message: "What is your GitHub username?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
-];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
